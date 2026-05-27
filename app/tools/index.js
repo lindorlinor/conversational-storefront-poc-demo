5da8409592a4e74b27d1575b0b6299f0191e4f99
Lindor linor <lindorlinor@gmail.com>
2026-05-27 12:11:20 +0200

    opz avevo sbagliato un import

--- app/tools/index.js
+++ app/tools/index.js
@@ -1,8 +1,8 @@
 import { tool } from 'ai';
 import { searchProductDefinition } from './search-product/definition';
 import { searchProductExecute } from './search-product/execute';
-import { fetchCollectionDefinition } from './search-product/definition';
-import { fetchCollectionExecute } from './search-product/execute';
+import { fetchCollectionDefinition } from './fetch-collection/definition';
+import { fetchCollectionExecute } from './fetch-collection/execute';
 
 export const searchProductTool = tool({
   ...searchProductDefinition,
